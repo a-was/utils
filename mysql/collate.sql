6d1addcd01d07b52a27b996de1fd820f835e2316
a-was <adam.wasilewski@unix.com.pl>
2023-08-25 21:33:42 +0200

    mysql: Add CREATE DATABASE

--- mysql/collate.sql
+++ mysql/collate.sql
@@ -1,3 +1,4 @@
 SELECT schema_name, default_character_set_name, default_collation_name FROM information_schema.SCHEMATA;
 
+CREATE DATABASE IF NOT EXISTS db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER DATABASE db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
